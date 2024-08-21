87419b4f8abe63f68eb64961bc7cbf0c2dd8e55c
bishal2408 <bishallamichhane24@gmail.com>
2024-08-21 22:27:43 +0545

    upload image from device functionality added

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -40,12 +40,28 @@ const PlacesPage = () => {
 
     const addPhotoByLink = async (event) => {
         event.preventDefault()
-        const {data:filename} = await axios.post('upload-by-link', {link:photoLink})
+        const { data: filename } = await axios.post('upload-by-link', { link: photoLink })
         setAddedPhotos(prev => {
             return [...prev, filename]
         })
         setPhotoLink('')
-    }   
+    }
+
+    const uploadPhoto = (event) => {
+        const files = event.target.files
+        const data = new FormData()
+        for(let i=0; i < files.length ; i++) {
+            data.append('photos', files[i])
+        }
+        axios.post('/upload', data, {
+            headers: {'Content-Type': 'multipart/form-data'}
+        }).then(response => {
+            const {data:filenames} = response
+            setAddedPhotos(prev => {
+                return [...prev, ...filenames]
+            })
+        })
+    }
 
     return (
         <div>
@@ -74,7 +90,6 @@ const PlacesPage = () => {
                             value={address}
                             onChange={event => setAddress(event.target.value)}
                             placeholder='address' />
-
                         {preInput('Photos', 'more = better')}
                         <div className='flex gap-2'>
                             <input type="text"
@@ -85,17 +100,18 @@ const PlacesPage = () => {
                         </div>
 
                         <div className='mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
-                            {addedPhotos.length > 0 && addedPhotos.map(link => (
-                                <div>
-                                    <img className='rounded-2xl ' src={'http://localhost:5555/uploads/' + link} />
+                            {addedPhotos.length > 0 && addedPhotos.map((link, index) => (
+                                <div key={index} className='h-32 flex'>
+                                    <img className='rounded-2xl w-full object-cover' src={'http://localhost:5555/uploads/' + link} />
                                 </div>
                             ))}
-                            <button className='flex items-center justify-center gap-1 border bg-transparent rounded-2xl p-2 text-2xl text-gray-600'>
+                            <label className='flex items-center gap-1 justify-center cursor-pointer border bg-transparent rounded-2xl p-2 text-2xl text-gray-600 h-32'>
+                                <input type="file" multiple style={{ display: 'none' }} onChange={uploadPhoto} />
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
                                 </svg>
                                 Upload
-                            </button>
+                            </label>
                         </div>
 
                         {preInput('Description', 'Description of the place')}
--- server/index.js
+++ server/index.js
@@ -3,30 +3,32 @@ import mongoose from "mongoose";
 import cors from "cors";
 import "dotenv/config";
 import bcrypt from "bcryptjs";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
-import imageDownloader from 'image-downloader'
+import imageDownloader from "image-downloader";
+import multer from "multer";
+import fs from "fs";
 
 import { User } from "./models/User.js";
 
 const app = express();
 const bcryptSalt = bcrypt.genSaltSync(10);
 
-const __dirname = import.meta.dirname 
+const __dirname = import.meta.dirname;
 
 // middleware for parsing json body
 app.use(express.json());
 // middleware to read cookies
-app.use(cookieParser())
+app.use(cookieParser());
 // middleware to display everything inside /uploads directory
-app.use('/uploads', express.static(__dirname + '/uploads'))
+app.use("/uploads", express.static(__dirname + "/uploads"));
 // middleware to handle CORS policy
 app.use(
   cors({
     origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: "Content-Type",
-    credentials: true
+    credentials: true,
   })
 );
 
@@ -42,66 +44,82 @@ app.post("/register", async (req, res) => {
     });
 
     res.json(userDoc);
-
   } catch (error) {
     res.status(422).json(error);
   }
 });
 
-// endpoint to login a user using jwt 
-app.post('/login', async (req, res) => {
-  const {email, password} = req.body
+// endpoint to login a user using jwt
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-  const userDoc = await User.findOne({email})
-  if(userDoc) {
-    const passOk = bcrypt.compareSync(password, userDoc.password)
-    if(passOk) {
-      jwt.sign({
-        email: userDoc.email,
-        id: userDoc._id,
-      }, process.env.JWT_SECRET, {}, (err, token) => {
-        if (err) throw err
-        res.cookie('token', token).json(userDoc)
-      })
+  const userDoc = await User.findOne({ email });
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign(
+        {
+          email: userDoc.email,
+          id: userDoc._id,
+        },
+        process.env.JWT_SECRET,
+        {},
+        (err, token) => {
+          if (err) throw err;
+          res.cookie("token", token).json(userDoc);
+        }
+      );
     } else {
-      res.status(422).json('pass not ok')
+      res.status(422).json("pass not ok");
     }
-  }else {
-    res.json("Not found")
+  } else {
+    res.json("Not found");
   }
-})
+});
 
 // endpoint to get user profile
-app.get('/profile', (req, res) => {
-  const {token} = req.cookies
-  if(token) {
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  if (token) {
     jwt.verify(token, process.env.JWT_SECRET, {}, async (err, userData) => {
       if (err) throw err;
-      const {name, email, _id} = await User.findById(userData.id)
-      res.json({name, email, _id})
-    })
+      const { name, email, _id } = await User.findById(userData.id);
+      res.json({ name, email, _id });
+    });
   } else {
-    res.json(null)
+    res.json(null);
   }
-})
+});
 
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json(true);
+});
 
-
-app.post('/logout', (req, res) => {
-  res.cookie('token', '').json(true)
-})
-
-
-app.post('/upload-by-link', async (req, res) => {
-  const {link} = req.body
-  const newName = 'photo' + Date.now() + '.jpg'
+app.post("/upload-by-link", async (req, res) => {
+  const { link } = req.body;
+  const newName = "photo" + Date.now() + ".jpg";
   await imageDownloader.image({
     url: link,
-    dest: __dirname + '/uploads/' + newName,
-  })
+    dest: __dirname + "/uploads/" + newName,
+  });
 
-  res.json(newName)
-})
+  res.json(newName);
+});
+
+const photosMiddleware = multer({ dest: "uploads/" });
+app.post("/upload", photosMiddleware.array("photos", 100), (req, res) => {
+  const uploadedFiles = [];
+  for (let i = 0; i < req.files.length; i++) {
+    const { path, originalname } = req.files[i];
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    const newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+    uploadedFiles.push(newPath.replace('uploads\\', ''))
+  }
+  console.log(uploadedFiles)
+  res.json(uploadedFiles);
+});
 
 // connecting to mongodb database
 mongoose
